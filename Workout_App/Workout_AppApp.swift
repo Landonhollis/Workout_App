741bbbe9242a04ea1371a0cb5bd3d699cb3374c3
Landonhollis <landonhollis2006@gmail.com>
2025-01-26 13:09:48 -0500

    Initial Commit

---
+++ Workout_App/Workout_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_AppApp.swift
+//  Workout_App
+//
+//  Created by Landon Hollis on 1/26/25.
+//
+
+import SwiftUI
+
+@main
+struct Workout_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Workout_AppTests/Workout_AppTests.swift
@@ -0,0 +1,17 @@
+//
+//  Workout_AppTests.swift
+//  Workout_AppTests
+//
+//  Created by Landon Hollis on 1/26/25.
+//
+
+import Testing
+@testable import Workout_App
+
+struct Workout_AppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
